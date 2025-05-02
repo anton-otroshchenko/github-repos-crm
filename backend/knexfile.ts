a1ffe1e0b1bd9e4a166cd8d6d5469ee1a73f5b05
anton-otroshchenko <anton3447@gmail.com>
2025-05-02 21:00:41 +0300

    add prettier

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -1,31 +1,31 @@
-import { knexSnakeCaseMappers } from 'objection';
-import { config } from './src/libs/config/config.ts'
+import { knexSnakeCaseMappers } from "objection";
+import { config } from "./src/libs/config/config.ts";
 
 const {
-  DATABASE: database,
-  USERNAME: username,
-  PASSWORD: password,
-  HOST: host,
-  PORT: port,
-  CLIENT: client,
-  DEBUG: debug
+	DATABASE: database,
+	USERNAME: username,
+	PASSWORD: password,
+	HOST: host,
+	PORT: port,
+	CLIENT: client,
+	DEBUG: debug,
 } = config.DB;
 
 const knexConfig = {
-  client,
-  connection: {
-    user: username,
-    port: parseInt(port as string, 10),
-    host,
-    database,
-    password
-  },
-  migrations: {
-    directory: './src/db/migrations',
-    tableName: 'knex_migrations'
-  },
-  debug,
-  ...knexSnakeCaseMappers({ underscoreBetweenUppercaseLetters: true })
+	client,
+	connection: {
+		user: username,
+		port: parseInt(port as string, 10),
+		host,
+		database,
+		password,
+	},
+	migrations: {
+		directory: "./src/db/migrations",
+		tableName: "knex_migrations",
+	},
+	debug,
+	...knexSnakeCaseMappers({ underscoreBetweenUppercaseLetters: true }),
 };
 
 export default knexConfig;
